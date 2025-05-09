25e04222e3a58aa61e8a4091f94fdf80ff03ad32
Minimons <minimons@r35157.crypto>
2025-05-09 12:08:23 +0200

    Add support for local maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,17 @@
 plugins {
     java
+    `maven-publish`
+}
+
+group = "crypto.r35157.nenjim"
+version = "0.1.0-SNAPSHOT"
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
 }
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
 }
 
 tasks.test {
@@ -23,4 +27,18 @@ java {
 tasks.withType<JavaCompile> {
     sourceCompatibility = "24"
     targetCompatibility = "24"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri(System.getProperty("user.home") + "/.m2/repository")
+        }
+    }
 }
